5990d0076b72a513418aebf61389fe15b28f344a
lenathecoder <lena.m.larson@gmail.com>
2025-04-13 17:54:20 -0700

    WallJump finished tutorial!

--- Lena Test Project/Assets/Scripts/PlayerMovement.cs
+++ Lena Test Project/Assets/Scripts/PlayerMovement.cs
@@ -10,10 +10,11 @@ public class PlayerMovement : MonoBehaviour
     private Animator anim;
     private BoxCollider2D boxCollider;
     private float wallJumpCooldown;
+    private float horizontalInput;
 
     private void Awake()
     {
-        //Grab references for rigidbody and animaor from object
+        //Grab references for rigidbody and animator from object
         body = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
         boxCollider = GetComponent<BoxCollider2D>();
@@ -21,17 +22,13 @@ public class PlayerMovement : MonoBehaviour
 
     private void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        body.linearVelocity = new Vector2(horizontalInput*speed, body.linearVelocity.y);
+        horizontalInput = Input.GetAxis("Horizontal");
 
         //Flip player when moving left/right
         if(horizontalInput>.01f)
             transform.localScale = Vector2.one;
         else if(horizontalInput <-.01f)
             transform.localScale = new Vector2(-1, 1);
-
-        if(Input.GetKey(KeyCode.Space) && isGrounded())
-            Jump();
 
         //Set animator parameters
         anim.SetBool("run", horizontalInput != 0);
@@ -42,20 +39,20 @@ public class PlayerMovement : MonoBehaviour
         if(wallJumpCooldown > .2f)
         {
             body.linearVelocity = new Vector2(horizontalInput*speed, body.linearVelocity.y);
-
-            if(onWall() && isGrounded())
+            
+            if(onWall() && !isGrounded())
             {
                 body.gravityScale = 0;
                 body.linearVelocity = Vector2.zero;
             }
             else
                 body.gravityScale = 7;
-
-            if(Input.GetKey(KeyCode.Space))
-                Jump();
         }
         else
             wallJumpCooldown += Time.deltaTime;
+            
+        if(Input.GetKey(KeyCode.Space))
+            Jump();
     }
 
     private void Jump()
@@ -63,18 +60,22 @@ public class PlayerMovement : MonoBehaviour
         if(isGrounded())
         {
             body.linearVelocity = new Vector2(body.linearVelocity.x, jumpPower);
-            anim.SetTrigger("jump");
+            anim.SetTrigger("jump");      
         }
-       else if(onWall() && isGrounded())
-       {
+        else if(onWall() && !isGrounded())
+        {
+            if(horizontalInput == 0)
+            {
+                body.linearVelocity = new Vector2(-Mathf.Sign(transform.localScale.x) * 10, 0); 
+                transform.localScale = new Vector2(-Mathf.Sign(transform.localScale.x), transform.localScale.y);
+            }
+            else
+                body.linearVelocity = new Vector2(-Mathf.Sign(transform.localScale.x) * 3, 6);
+
             wallJumpCooldown = 0;
-            body.linearVelocity = new Vector2(-Mathf.Sign(transform.localScale.x) * 3, 6);
-       }
+        }
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-    }
     private bool isGrounded()
     {
         RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, Vector2.down, 0.1f, groundLayer);
